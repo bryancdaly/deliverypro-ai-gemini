f40d586100d885dd22eebedc85261d2f5a5fb36b
Bryan Daly <bryan.daly@outlook.com>
2026-06-01 18:45:08 +1200

    include legacy workspace assets in deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    outputFileTracingIncludes: {
+        "/legacy/[...path]": ["./legacy/**/*"]
+    }
 };
 
 export default nextConfig;
